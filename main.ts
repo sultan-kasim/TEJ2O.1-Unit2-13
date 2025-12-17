88d53207636da2842abf2faa14a7edb991278614
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-12-17 12:48:11 -0500

    Recieves it

--- main.ts
+++ main.ts
@@ -4,5 +4,16 @@
  * Created on: Sep 2020
  * This program ...
 */
+// setup
+radio.setGroup(15)
+basic.showIcon(IconNames.Happy)
 
-basic.showString('Hello, World!')
+// receive messages
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.pause(500)
+    basic.showIcon(IconNames.Happy)
+   
+})
